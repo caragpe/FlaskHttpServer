ee620d50f0ec37d0f950e3e18e93e9f349392a27
Carlos Agullo <carlos.agullo@7shifts.com>
2020-03-25 23:01:16 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import request, jsonify
+from app.errors import InvalidUsage
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Test"
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route('/echo')
+def echo():
+    if request.method != 'GET':
+        raise InvalidUsage('METHOD NOT ALLOWED', 405)
+    pass
+
+
+@app.route('/set_banner', methods=['POST'])
+def set_banner():
+    if request.method != 'POST':
+        raise InvalidUsage('METHOD NOT ALLOWED', 405)
+    pass
