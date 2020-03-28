a7ad2f77bd0556c3139cc5434548df1c70c44db4
Carlos Agullo <carlos.agullo@7shifts.com>
2020-03-28 15:50:25 -0400

    Improving tests

--- test/test_echoservice.py
+++ test/test_echoservice.py
@@ -7,23 +7,23 @@ class TestEchoService:
         with app.test_client() as c:
             rv = c.get('/echo?message=Hello')
             assert rv.status_code == 200
-            assert rv.data == b'Hello'
+            assert rv.data.decode('utf-8') == 'Hello'
 
     def test_fails_no_message_param_send(self):
         with app.test_client() as c:
             rv = c.get('/echo')
             assert rv.status_code == 406
-            assert b'NOT ACCEPTABLE' in rv.data
+            assert 'NOT ACCEPTABLE' in rv.status
 
     def test_fails_multiple_param_send(self):
         with app.test_client() as c:
             rv = c.get('/echo?message=Hello&from=Carlos')
             assert rv.status_code == 406
-            assert b'NOT ACCEPTABLE' in rv.data
+            assert 'NOT ACCEPTABLE' in rv.status
 
     def test_fails_when_http_method_distinct_from_get(self):
         with app.test_client() as c:
             rv = c.post('/echo?message=Hello&from=Carlos')
             assert rv.status_code == 405
-            print(rv.data)
-            # assert b'Method not allowed' in rv.data/
+            assert 'METHOD NOT ALLOWED' in rv.status
+
